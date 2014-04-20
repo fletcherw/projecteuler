e02ff73f23225477f74ccc3aeeaaf7939316577c
Fletcher Woodruff <fletcherwoodruff@gmail.com>
2014-04-20 08:10:46 -0700

    Solved coin sums

--- CoinSums.py
+++ CoinSums.py
@@ -1,20 +1,16 @@
-#coins = [1, 2, 5, 10, 20, 50, 100]
-coins = [1, 2, 5]
+coins = [1, 2, 5, 10, 20, 50, 100]
 
-count = 0 #for a 200 pence coin
+count = 1 #for a 200 pence coin
 
 def makeSum(goal, set):
-	if goal
-	if (len(set) == 1): return 1 if (goal % set[0] == 0) else 0
+	if (goal == 0): return 1
+	if (len(set) == 1): return 1 if goal % (set[0]) == 0 else 0
 	ways = 0
-	current = 0
-	large = set[len(set) - 1]
-	while current < goal:
-		ways += makeSum(goal - current, set[:-1])
-		current += large
+	for n in range(0, goal + 1, set.pop()):
+		ways += makeSum(goal - n, set[:])	
 	return ways
 
-count += makeSum(10, coins)
+count += makeSum(200, list(coins))
 
 print count
 	
